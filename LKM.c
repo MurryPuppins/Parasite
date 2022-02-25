e30d75cc43e8ad5209f5e3adb097844931dc0ac8
Cameron C <cjcarey314@gmail.com>
2022-02-25 00:55:59 -0500

    Test files
    Simple test files, reference found inside LKM.c

---
+++ LKM.c
@@ -0,0 +1,61 @@
+/*****************************************************
+* This is just test code, establishing a baseline for this project
+* Reference: https://infosecwriteups.com/linux-kernel-communication-part-1-netfilter-hooks-15c07a5a5c4e
+ *****************************************************/
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+
+static struct nf_hook_ops *nfho = NULL;
+
+// Hook function, does the schtuff
+static unsigned int hfunc(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
+{
+	struct iphdr *iph;
+	struct udphdr *udph;
+	if (!skb)
+		return NF_ACCEPT;
+
+	iph = ip_hdr(skb);
+	if (iph->protocol == IPPROTO_TCP) {
+		tcph = tcp_hdr(skb);
+		if (ntohs(tcph->dest) == 22) {
+			return NF_ACCEPT;
+		}
+        else if (ntohs(tcph->dest) == 80) {
+            return NF_DROP;
+        }
+	}
+	
+	return NF_ACCEPT;
+}
+
+// Initializing function for loading the module into the kernel
+static int __init LKM_init(void)
+{
+	nfho = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
+	
+	/* Initialize netfilter hook */
+	nfho->hook 	= (nf_hookfn*)hfunc;		/* hook function */
+	nfho->hooknum 	= NF_INET_PRE_ROUTING;		/* received packets */
+	nfho->pf 	= PF_INET;			/* IPv4 */
+	nfho->priority 	= NF_IP_PRI_FIRST;		/* max hook priority */
+	
+	nf_register_net_hook(&init_net, nfho);
+}
+
+// Uninitializes/removes the LKM
+static void __exit LKM_exit(void)
+{
+	nf_unregister_net_hook(&init_net, nfho);
+	kfree(nfho);
+}
+
+module_init(LKM_init);
+module_exit(LKM_exit);
---
+++ Makefile
@@ -0,0 +1,9 @@
+obj-m := LKM.o
+LKM-objs += simple_netfilter_LKM.o
+
+all:
+ make -C /lib/modules/$(shell uname -r)/build M=$(shell pwd) modules
+ rm -r -f *.mod.c .*.cmd *.symvers *.o
+
+clean:
+ make -C /lib/modules/$(shell uname -r)/build M=$(shell pwd) clean
